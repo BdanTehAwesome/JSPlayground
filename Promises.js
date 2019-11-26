388754e45fbd504fd02815e18acfa15baeaad578
Brendan Delaney <brendandelaneyirl@gmail.com>
2019-11-26 00:07:46 +0000

    first commit

---
+++ Promises.js
@@ -0,0 +1,25 @@
+function promisTest() {
+    let test1 = false
+    let test2 = true
+    return new Promise((resolve, reject) => {
+      if (test1) {
+        reject({
+          name: 'Sorry', 
+          message: 'Denied 1'
+        })
+      } else if (test2) {
+        reject({
+          name: 'Sorry',
+          message: 'Denied 2' 
+        })
+      } else {
+        resolve('Success')
+      }
+    })
+  }
+
+  promisTest().then(message => {
+    console.log('Success' + message);
+  }).catch((error) => {
+    console.log(error.name + ' ' + error.message)
+  })
